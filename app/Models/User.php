9d56fbc88bca5adb5bf90533daca2da230c7c558
Duvan Cordoba <duvan.cordoba@blaucastmedia.com>
2025-02-08 10:27:32 -0500

    Add: new models, migrations and seeders

--- app/Models/User.php
+++ app/Models/User.php
@@ -65,4 +65,24 @@ class User extends Authenticatable
     {
         return $this->belongsTo(City::class);
     }
+
+    public function calendars()
+    {
+        return $this->belongsToMany(Calendar::class);
+    }
+
+    public function departaments()
+    {
+        return $this->belongsToMany(Departament::class);
+    }
+
+    public function holidays()
+    {
+        return $this->hasMany(Holiday::class);
+    }
+
+    public function timeSheets()
+    {
+        return $this->hasMany(TimeSheet::class);
+    }
 }
